08accba9181210f650976fe38726877f57d33388
John-Patrick-Macalalad <81607064+John-Patrick-Macalalad@users.noreply.github.com>
2021-06-28 01:00:03 +0800

    Add files via upload

---
+++ OnlineShoppingApplication.BL/UserAuthorization.cs
@@ -0,0 +1,58 @@
+﻿using OnlineShoppingApplication.DL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShoppingApplication.BL
+{
+    public class UserAuthorization
+    {
+        public List<User> UserCredentials = new List<User>()
+        {
+            new User("MACSHOP","2021")
+        };
+
+        public void Login()
+        {
+            UserCategory catalogue = new UserCategory();
+            bool successful = false;
+            Console.WriteLine("");
+            Console.WriteLine("Enter UserName:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Enter PassWord:");
+            string passWord = Console.ReadLine();
+
+            foreach (User user in UserCredentials)
+            {
+                if (userName == user.UserName && passWord == user.PassWord)
+                {
+                    Console.WriteLine("Logged in Successfully!!");
+                    successful = true;
+                    catalogue.Category();
+                }
+            }
+            if (!successful)
+            {
+                Console.WriteLine(" ! LOGIN FAILED ! ");
+            }
+        }
+        public void Signup()
+        {
+            Console.WriteLine("Enter UserName: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Enter PassWord: ");
+            string passWord = Console.ReadLine();
+
+            UserCredentials.Add(new User(userName, passWord));
+            Console.WriteLine("|==========================================|");
+            Console.WriteLine("|         REGISTERED SUCCESSFULLY!!!       |");
+            Console.WriteLine("|==========================================|");
+
+            foreach (User user in UserCredentials)
+            {
+                Console.WriteLine("Username:{0}, Password:{1}", user.UserName, user.PassWord);
+            }
+
+        }
+    }
+}
---
+++ OnlineShoppingApplication.BL/UserCategory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShoppingApplication.BL
+{
+    public class UserCategory
+    {
+        public void Category()
+        {
+            Inventory productServices = new Inventory();
+
+        START:
+            Console.WriteLine("|==========================================|");
+            Console.WriteLine("|           (1) View a Product             |");
+            Console.WriteLine("|           (2) Add a Product              |");
+            Console.WriteLine("|           (3) Remove a Product           |");
+            Console.WriteLine("|           (4) Exit                       |");
+            Console.WriteLine("|==========================================|");
+            int userInput = int.Parse(Console.ReadLine());
+            switch (userInput)
+            {
+                case 1:
+                    productServices.ProductView();
+                    goto START;
+                case 2:
+                    productServices.ProductAdd();
+                    goto START;
+                case 3:
+                    productServices.ProductRemoved();
+                    goto START;
+
+                default:
+                    break;
+            }
+
+        }
+    }
+}
