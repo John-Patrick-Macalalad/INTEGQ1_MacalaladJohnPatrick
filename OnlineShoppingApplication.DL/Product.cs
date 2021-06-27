435c4871d00fb0334e6ea3944c825637b07e77a6
John-Patrick-Macalalad <81607064+John-Patrick-Macalalad@users.noreply.github.com>
2021-06-28 00:51:09 +0800

    Add files via upload

---
+++ OnlineShoppingApplication.DL/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OnlineShoppingApplication.DL
+{
+    public class Product
+    {
+        public string keyboardBrand;
+        private string price;
+        private DateTime date;
+
+        public Product(string keyboardBrand, string price, DateTime date)
+        {
+            this.keyboardBrand = keyboardBrand;
+            this.price = price;
+            this.date = date;
+        }
+
+        public string KeyboardBrand
+        {
+            get { return keyboardBrand; }
+            set { keyboardBrand = value; }
+        }
+
+        public string Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+    }
+}
