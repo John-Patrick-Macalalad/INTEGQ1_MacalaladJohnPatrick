81226bb77027b6a52f5f751e8b6e415a9f3ce847
John-Patrick-Macalalad <jpmac321@gmail.com>
2021-06-27 21:19:29 +0800

    initial commit - integq1

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using OnlineShoppingApplication.BL;
+using System;
+
+namespace OnlineShoppingApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserAuthorization user = new UserAuthorization();
+
+            Console.WriteLine("|==========================================|");
+            Console.WriteLine("|    Welcome to MAC Shop! Please login.    |");
+            Console.WriteLine("|==========================================|");
+            Console.WriteLine("|                                          |");
+            Console.WriteLine("|               (1) LOGIN                  |");
+            Console.WriteLine("|__________________________________________|");
+            Console.WriteLine("|New to MAC SHOP?                          |");
+            Console.WriteLine("|               (2) SIGNUP                 |");
+            Console.WriteLine("|                                          |");
+            Console.WriteLine("|==========================================|");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput.Equals(1))
+            {
+                user.Login();
+            }
+            else if (userInput.Equals(2))
+            {
+                user.Signup();
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
