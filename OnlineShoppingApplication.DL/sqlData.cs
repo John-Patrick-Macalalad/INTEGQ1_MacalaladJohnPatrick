acd8e03bf84b8e3d4e3b30d2266e2bca05deda1b
John-Patrick-Macalalad <81607064+John-Patrick-Macalalad@users.noreply.github.com>
2021-06-28 04:29:04 +0800

    Add files via upload

---
+++ OnlineShoppingApplication.DL/sqlData.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Common;
+using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShoppingApplication.DL
+{
+    class sqlData
+    {
+        static string connectionString = "Data Source=JAYPEE;Initial Catalog=OnlineShoppingApplication;Integrated Security=True";
+        static SqlConnection sqlConnection = new SqlConnection(connectionString);
+    }
+}
